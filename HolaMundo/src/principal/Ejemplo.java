1275d807a2f3e4de1c525c2b1789d639cda87a58
Minato97 <ignacio.andrade8033@alumnos.udg.mx>
2021-11-02 21:40:06 -0600

    Tercer commit

---
+++ HolaMundo/src/principal/Ejemplo.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Ejemplo {
+    public static void main(String[] args){
+        System.out.println("Hola mundo...");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+         }
+        
+    }
+}
---
+++ HolaMundo/src/principal/NumeroUnoAMil.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class NumeroUnoAMil {
+    public static void main(String[] args){
+        for(int i = 1; i <= 1000; i++){
+            System.out.println(i);
+        }
+    }
+}
